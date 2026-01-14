b7326ca48261188fd72feee21f42e10583fe8b1d
Khafit Badrus Zaman <khafitbadruszaman@gmail.com>
2026-01-14 10:10:02 +0700

    add file

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,13 @@
+import HeroSection from "./component/home/hero";
+import CategoriesSection from "./component/home/categories";
+import ProductsSection from "./component/home/products";
+
+export default function Home() {
+  return (
+    <main>
+      <HeroSection />
+      <CategoriesSection />
+      <ProductsSection />
+    </main>
+  );
+}
